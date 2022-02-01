1b93cb4ec0a68a318c70f36df56e514fcfd21a97
Sanil Davis <sanil.davis@speehive.com>
2022-02-01 21:44:27 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmpManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddData(string n1,string n2,string n3)
+        {
+            return View();
+        }
+    }
+}
